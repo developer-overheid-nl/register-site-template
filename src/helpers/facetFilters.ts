7abd0feb09ab9509d4750846cd4069e1c7938902
Jaap-Hein Wester <j.h.wester@setonix.nl>
2026-06-24 12:32:44 +0200

    WIP: added facet filters and tags

--- src/helpers/facetFilters.ts
+++ src/helpers/facetFilters.ts
@@ -1,16 +1,40 @@
+/**
+ * @file This file is a helper file to aid with the dingen-api.
+ * It should not be needed if used with a don-*-api, but if your api is different you might need similiar helpers.
+ */
+
 import type { FilterData } from "@developer-overheid-nl/don-register-components";
 
 /** Capitalize character at index */
-const capitalizeCharAt = (str: string, at = 0): string => (
-  str.slice(0, at) + str.charAt(at).toUpperCase() + str.slice(at + 1)
-)
+const capitalizeCharAt = (str: string, at = 0): string =>
+  str.slice(0, at) + str.charAt(at).toUpperCase() + str.slice(at + 1);
 
-export const parseTags = (tags: Record<string, number>): FilterData => {
+/** Remove empty records from object */
+export const removeEmptyValues = (
+  obj: Record<string, string | string[] | number>,
+): Record<string, string | string[] | number> => {
+  const newObj: Record<string, string | string[] | number> = {};
+  for (const [key, value] of Object.entries(obj)) {
+    if (value !== "" && !(Array.isArray(value) && value.length === 0)) {
+      newObj[key] = value;
+    }
+  }
+  return newObj;
+};
+
+export const parseTags = (
+  tags?: Record<string, number> | null,
+  queryObject?: Record<string, string | string[]>,
+): FilterData | [] => {
+  if (!tags) return [];
+
+  const filterkey = "tags_like";
+
   const options = Object.entries(tags).map(([key, value]) => ({
     value: key,
     label: capitalizeCharAt(key),
     count: value,
-    selected: false,
+    selected: queryObject?.[filterkey]?.includes(key),
   }));
 
   return {
@@ -20,24 +44,31 @@ export const parseTags = (tags: Record<string, number>): FilterData => {
     type: "multi-select" as FilterData["type"],
     options,
   };
-}
+};
 
-export const parseCategories = (categories?: {id: number, name: string, description: string, amount?: number}[]): FilterData | [] => {
+export const parseCategories = (
+  categories?:
+    | { id: number; name: string; description: string; amount?: number }[]
+    | null,
+  queryObject?: Record<string, string | string[]>,
+): FilterData | [] => {
   if (!categories) return [];
-  
-  const options = categories.map(({name, description, amount}) => ({
+
+  const filterkey = "category";
+
+  const options = categories.map(({ name, description, amount }) => ({
     value: name,
     label: name,
     description,
     count: Number(amount),
-    selected: false,
+    selected: queryObject && queryObject[filterkey] === name,
   }));
 
   return {
-    key: "category",
+    key: filterkey,
     label: "Categorieën",
     description: "Filter op categorieën",
     type: "single-select" as FilterData["type"],
     options,
   };
-}
+};
